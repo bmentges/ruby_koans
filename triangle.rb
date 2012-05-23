f2797bee6eb868e14eae12269519b0cf561fc250
Bruno Carvalho <bmentges@gmail.com>
2012-05-23 10:38:58 -0300

    153/280

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  results = [a == b, a == c, b == c]
+
+  if results.count(true) == 3
+    :equilateral
+  else 
+    if results.count(true) == 1
+      :isosceles
+    else
+      :scalene
+    end
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
